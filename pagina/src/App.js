353fff066dde3b79035b4dd1ca957f400c5ce606
sabbaggh <137013454+sabbaggh@users.noreply.github.com>
2024-12-05 01:29:10 -0600

    scroll-snap

--- pagina/src/App.js
+++ pagina/src/App.js
@@ -27,19 +27,19 @@ function App() {
 
     return;
   };
-  window.addEventListener("scroll", isScrollingDown);
+  //window.addEventListener("scroll", isScrollingDown);
   return (
-    <div className="flex-1 h-screen bg-gray-950 w-full">
+    <div className="flex-1 h-screen bg-gray-950 w-full snap-y snap-mandatory overflow-y-auto z-90 absolute">
       <div className='flex flex-row w-full h-20 bg-gray-950 items-center justify-center fixed z-50'>
         <a href='#inicio' className='text-slate-50 opacity-100 mr-8 text-xl hover:text-teal-300'>Inicio</a>
         <a href='#conocenos' className='text-slate-50 opacity-100 text-xl hover:text-teal-300'>Conocenos</a>
       </div>
-      <div className='h-screen flex items-center justify-center w-full' id='inicio'>
+      <div className='h-screen flex items-center justify-center w-full snap-start' id='inicio'>
         <img src={require('./images/equipo1.jpg')} alt='equipo1' className='w-full h-screen object-cover blur-sm'/>
         <p className='text-white text-6xl z-20 absolute'>Mejora tu negocio</p>
         <div className='z-10 bg-gray-950 absolute h-full w-full opacity-70'></div>
       </div>
-      <div className='flex flex-row h-screen items-center justify-center w-full bg-gray-950 space-x-8' id='conocenos'>
+      <div className='flex flex-row h-screen items-center justify-center w-full bg-gray-950 space-x-8 snap-start' id='conocenos'>
         <div className='text-slate-50 w-2/5'>
           <h1 className='text-3xl text-center'>Sobre Inndomitus</h1>
           <p className='text-2xl'>Inndomitus se encarga de implementar soluciones de IA para las empresas</p>
